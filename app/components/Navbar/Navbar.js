df06fd25b26c56ea33040ad505c7f906c2351708
Patrik Stas <patrik.stas@gmail.com>
2018-12-29 20:15:33 +0100

    Update next, node-sass. Override _app.js

--- app/components/Navbar/Navbar.js
+++ app/components/Navbar/Navbar.js
@@ -1,39 +1,41 @@
-import React, { Component } from "react";
-import Link from "next/link";
+import React, {Component} from "react";
 import "./Navbar.scss";
+import {Input, Menu, Segment} from 'semantic-ui-react'
+import Router from 'next/router'
+
 
 class Navbar extends Component {
-  constructor(props) {
-    super(props);
-    this.props = props;
-  }
+    constructor(props) {
+        super(props);
+        this.props = props;
+        this.state = {activePage:''}
+    }
 
-  render() {
-    return (
-      <nav>
-        <div>
-          <Link href="/">
-            <a title="Our API">Home</a>
-          </Link>
-          <Link href="/tx-domain">
-            <a title="About Next JS">Domain</a>
-          </Link>
+    async componentDidMount() {
+        console.log(`Navbar mounted`);
+        console.log(Router.pathname);
+        console.log(Router.route);
+    }
+    //
+    // static async getInitialProps({req, query}) {
+    //     // const baseUrl = this.getBaseUrl(req);
+    //     console.log('navbar initial props')
+    //     console.log(JSON.stringify(req))
+    //     // const domainTxs = await this.getLastDomainTx(baseUrl);
+    // }
 
-          <Link href="/tx-network">
-              <a title="About Next JS">Network</a>
-          </Link>
-
-            <Link href="/tx-config">
-                <a title="About Next JS">Config</a>
-            </Link>
-
-          <Link href="/about">
-              <a title="About Next JS">About</a>
-          </Link>
-        </div>
-      </nav>
-    );
-  }
+    render() {
+        return (
+            <nav>
+                <Menu pointing>
+                    <Menu.Item as="a" href="/" name='home' />
+                    <Menu.Item as="a" href="/tx-domain" name='domain' />
+                    <Menu.Item as="a" href="/tx-pool" name='pool'/>
+                    <Menu.Item as="a" href="/tx-config" name='config'/>
+                </Menu>
+            </nav>
+        );
+    }
 }
 
 export default Navbar;
