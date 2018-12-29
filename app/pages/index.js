8f9efd40ee5c82e411cbcf368ab303dc64c4eb22
Patrik Stas <patrik.stas@gmail.com>
2018-12-29 01:24:31 +0100

    wip: refactor

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,15 +1,13 @@
 import Head from "next/head";
 import {Component} from 'react';
 import "../scss/style.scss";
-import Navbar from "../components/Navbar";
+import Navbar from "../components/Navbar/Navbar";
 import fetch from 'isomorphic-unfetch'
-import TxPreview from "../components/TxPreview";
+import TxPreview from "../components/TxPreview/TxPreview";
 import Pagination from 'rc-pagination';
 import {Container} from 'semantic-ui-react';
-// const charts = require('chart.js');
-import {Line} from 'react-chartjs-2';
-import _ from 'lodash'
-import format from 'date-format';
+import TxsChart from "../components/TxChart/TxChart";
+import {getTimeseriesConfig, getTimeseriesDomain, getTimeseriesPool} from "../api-client";
 
 class MainPage extends Component {
 
@@ -22,70 +20,30 @@ class MainPage extends Component {
         return await res.json();
     }
 
-    static async getTimeseriesDomain(baseUrl) {
-        let res = await fetch(`${baseUrl}/api/tx-domain/timeseries`);
-        return await res.json();
-    }
-
-    static async getTimeseriesPool(baseUrl) {
-        let res = await fetch(`${baseUrl}/api/tx-pool/timeseries`);
-        return await res.json();
-    }
-
-    static async getTimeseriesConfig(baseUrl) {
-        let res = await fetch(`${baseUrl}/api/tx-config/timeseries`);
-        return await res.json();
-    }
 
     static async getInitialProps({req, query}) {
-        console.log(`getInitialProps`)
         const baseUrl = this.getBaseUrl(req);
-        console.log(`Get initial props is running!`);
         const domainTxs = await this.getLastDomainTx(baseUrl);
-        const timeseriesDomain = await this.getTimeseriesDomain(baseUrl);
-        const timeseriesPool = await this.getTimeseriesPool(baseUrl);
-        const timeseriesConfig = await this.getTimeseriesConfig(baseUrl);
+        const timeseriesDomain = await getTimeseriesDomain(baseUrl);
+        const timeseriesPool = await getTimeseriesPool(baseUrl);
+        const timeseriesConfig = await getTimeseriesConfig(baseUrl);
         return {
-            txs: domainTxs.txs,
             timeseriesDomain: timeseriesDomain.histogram,
-            timeseriesPool : timeseriesPool.histogram,
-            timeseriesConfig : timeseriesConfig .histogram
-        }
-    }
-
-    static createChartsDataset(timeSeries, label, borderColor) {
-        const zippedTimeseries = _.zip.apply(_, timeSeries);
-        const dates = zippedTimeseries[0].map(t=>format('yyyy.MM.dd', new Date(t)));
-        const txCnt = zippedTimeseries[1];
-        return {
-            labels: dates,
-            dataset: {
-                data: txCnt,
-                label,
-                borderColor,
-                fill: false
-            }
+            timeseriesPool: timeseriesPool.histogram,
+            timeseriesConfig: timeseriesConfig.histogram,
+            txs: domainTxs.txs
         }
     }
 
     render() {
-        const domain = MainPage.createChartsDataset(this.props.timeseriesDomain, "Domain tx count", "#3e95cd");
-        const pool = MainPage.createChartsDataset(this.props.timeseriesPool, "Pool tx count", "#cd4639");
-        const config = MainPage.createChartsDataset(this.props.timeseriesConfig, "Config tx count", "#3dcd34");
-        const data = {
-            labels: domain.labels,
-            datasets: [
-                domain.dataset,
-                pool.dataset,
-                config.dataset,
-            ]
-        };
         return (
             <Container>
                 <Head/>
                 <h1>Hyperldeger Indy Scan</h1>
+                <TxsChart timeseriesDomain={this.props.timeseriesDomain}
+                          timeseriesPool={this.props.timeseriesPool}
+                          timeseriesConfig={this.props.timeseriesConfig}/>
                 <Navbar/>
-                <Line data={data}/>
                 <div>
                     {this.props.txs.map(txn => <TxPreview tx={txn}/>)}
                 </div>
