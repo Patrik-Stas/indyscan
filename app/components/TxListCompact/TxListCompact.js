568de960082962c9f145546a7f59312e13221bc1
Patrik Stas <patrik.stas@absa.co.za>
2018-12-29 23:23:45 +0100

    Implement basic tx list component

---
+++ app/components/TxListCompact/TxListCompact.js
@@ -0,0 +1,31 @@
+import {Component} from 'react';
+import TxListItem from "../TxListItem/TxListItem";
+import {Grid, GridColumn, GridRow} from 'semantic-ui-react';
+import { Table } from 'semantic-ui-react'
+
+class TxListCompact extends Component {
+
+    render() {
+        return (
+            <Table striped>
+                <Table.Header>
+                    <Table.Row>
+                        <Table.HeaderCell>TxNo</Table.HeaderCell>
+                        <Table.HeaderCell>Type</Table.HeaderCell>
+                        <Table.HeaderCell>Timestamp</Table.HeaderCell>
+                        <Table.HeaderCell>ID</Table.HeaderCell>
+                    </Table.Row>
+                </Table.Header>
+                <Table.Body>
+                {this.props.txs.map((txn) => {
+                    return (
+                        <TxListItem key={txn.txnMetadata.seqNo} tx={txn}/>
+                    )
+                })}
+                </Table.Body>
+            </Table>
+        )
+    }
+}
+
+export default TxListCompact;
--- app/pages/tx-config.js
+++ app/pages/tx-config.js
@@ -1,11 +1,36 @@
-import React, {Component} from "react";
+import Head from "next/head";
+import "../scss/style.scss";
+import Navbar from "../components/Navbar/Navbar";
+import fetch from 'isomorphic-unfetch'
+import TxListCompact from "../components/TxListCompact/TxListCompact";
+import React, {Component} from 'react';
 
 class TxConfig extends Component {
 
+    static getBaseUrl(req) {
+        return req ? `${req.protocol}://${req.get('Host')}` : '';
+    }
+
+    static async getLastConfigTx(baseUrl) {
+        let res = await fetch(`${baseUrl}/api/tx-config`);
+        return await res.json();
+    }
+
+    static async getInitialProps({req, query}) {
+        console.log(`tx-config.js: Get initial props.`);
+        const baseUrl = this.getBaseUrl(req);
+        const domainTxs = await this.getLastConfigTx(baseUrl);
+        return {
+            txs: domainTxs.txs,
+        }
+    }
+
     render() {
         return (
-            <div>Tx list config</div>
-        );
+            <div>
+                <TxListCompact txs={this.props.txs}/>
+            </div>
+        )
     }
 }
 
