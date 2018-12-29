d574772f7b81663b36caee92f43ef00a337f75a8
Patrik Stas <patrik.stas@absa.co.za>
2018-12-29 21:41:18 +0100

    Add outline for other pages

---
+++ app/pages/tx-config.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+
+class TxConfig extends Component {
+
+    render() {
+        return (
+            <div>Tx list config</div>
+        );
+    }
+}
+
+export default TxConfig;
