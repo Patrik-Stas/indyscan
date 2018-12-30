e878c7ebd34af94538ff66b648df6f389cbb6274
Patrik Stas <patrik.stas@absa.co.za>
2018-12-30 13:00:18 +0100

    Scan multiple indy networks simultaneously.

--- daemon/index.js
+++ daemon/index.js
@@ -4,9 +4,8 @@ const sleep = require('sleep-promise');
 const createIndyClient = require('./indyclient');
 const storage = require ('indyscan-storage');
 
-const walletName = 'sovrinscan';
-const poolName = 'SOVRIN_MAINNET';
-const MAINNET_NETWORK = 'SOVRIN_MAINNET';
+const SOVRIN_MAINNET = 'SOVRIN_MAINNET';
+const SOVRIN_TESTNET = 'SOVRIN_TESTNET';
 
 // Connection URL
 const url = 'mongodb://localhost:27017';
@@ -15,39 +14,46 @@ const LEDGER_TYPE_POOL = '0';
 const LEDGER_TYPE_DOMAIN = '1';
 const LEDGER_TYPE_CONFIG = '2';
 
-const indyNetworks = [MAINNET_NETWORK];
+const indyNetworks = [SOVRIN_MAINNET, SOVRIN_TESTNET];
+
 
 async function run() {
-    const indyClient = await createIndyClient(poolName, walletName);
     storage.init(url, indyNetworks, async (storageManager, err) => {
         assert.equal(null, err);
         console.log(`Storage initialized. Storage url: ${url}. Indy networks ${JSON.stringify(indyNetworks)}`)
-        const txCollectionDomain = storageManager.getTxCollection(MAINNET_NETWORK, storage.txTypes.domain);
-        const txCollectionPool = storageManager.getTxCollection(MAINNET_NETWORK, storage.txTypes.pool);
-        const txCollectionConfig = storageManager.getTxCollection(MAINNET_NETWORK, storage.txTypes.config);
-        assert.equal(true, !!txCollectionDomain);
-        assert.equal(true, !!txCollectionPool);
-        assert.equal(true, !!txCollectionConfig);
-        console.log(`Tx collections ready. Let's scan.`);
-        await sleep(6*1000);
-        scanLedger(indyClient, txCollectionDomain, LEDGER_TYPE_DOMAIN, 'DOMAIN', 1, 15);
-        scanLedger(indyClient, txCollectionPool, LEDGER_TYPE_POOL, 'POOL', 1, 600);
-        scanLedger(indyClient, txCollectionConfig, LEDGER_TYPE_CONFIG, 'CONFIG', 1, 600);
+        for (let i=0; i<indyNetworks.length; i++) {
+            const NETWORK = indyNetworks[i];
+            console.log(`Getting ready indy client for Indy network: ${NETWORK}`)
+            const indyClient = await createIndyClient(NETWORK, `sovrinscan-${NETWORK}`);
+            const txCollectionDomain = storageManager.getTxCollection(NETWORK, storage.txTypes.domain);
+            const txCollectionPool   = storageManager.getTxCollection(NETWORK, storage.txTypes.pool);
+            const txCollectionConfig = storageManager.getTxCollection(NETWORK, storage.txTypes.config);
+            assert.equal(true, !!txCollectionDomain);
+            assert.equal(true, !!txCollectionPool);
+            assert.equal(true, !!txCollectionConfig);
+            console.log(`Tx collections for network ${NETWORK} ready. Scanning starts in few seconds.`);
+            setTimeout(()=> {
+                scanLedger(indyClient, txCollectionDomain, NETWORK, LEDGER_TYPE_DOMAIN, 'DOMAIN', 1, 15);
+                scanLedger(indyClient, txCollectionPool, NETWORK, LEDGER_TYPE_POOL, 'POOL', 1, 600);
+                scanLedger(indyClient, txCollectionConfig, NETWORK, LEDGER_TYPE_CONFIG, 'CONFIG', 1, 600);
+            }, 10*1000)
+        }
     }) ;
 }
 
-async function scanLedger(indyClient, txCollection, ledgerType, ledgerName, regularTimeoutSec, noNewTimeoutSec) {
+async function scanLedger(indyClient, txCollection, networkName, ledgerType, ledgerName, regularTimeoutSec, noNewTimeoutSec) {
     let txid = (await txCollection.findMaxTxIdInDb()) + 1;
     while (true) {
-        console.log(`[LedgerScan][${ledgerName}] Checking ${txid}th transaction.`);
+        const logPrefix = `[LedgerScan][${networkName}][${ledgerName}]`;
+        console.log(`${logPrefix} Checking ${txid}th transaction.`);
         const tx = await indyClient.getTx(txid, ledgerType);
         if (tx) {
-            console.log(`Retrieved tx ${txid}:\n${JSON.stringify(tx)}`);
+            console.log(`${logPrefix} Retrieved '${txid}'th tx:\n${txid}:\n${JSON.stringify(tx)}`);
             await txCollection.addTx(tx);
             await sleep(regularTimeoutSec*1000);
             txid++
         } else {
-            console.log(`Seems like tx ${txid} does not yet exist.`);
+            console.log(`${logPrefix} Seems '${txid}'th tx does not yet exist.`);
             await sleep(noNewTimeoutSec*1000)
         }
     }
