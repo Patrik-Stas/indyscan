3f4818753c91eff4fdd0ab564da6e651b24ff9cc
Patrik Stas <patrik.stas@absa.co.za>
2019-01-04 00:16:49 +0100

    Pretty date formatting

--- app/txtools/index.js
+++ app/txtools/index.js
@@ -1,9 +1,10 @@
+import moment from 'moment';
 
 export function extractTxInformation(tx) {
     const {type} = tx.txn
     const {txnId, txnTime, seqNo} = tx.txnMetadata;
     const timestamp = !!(txnTime)
-        ? new Date(txnTime * 1000).toISOString()
+        ? moment(txnTime * 1000).format('MMMM Do YYYY, h:mm:ss a')
         : "unknown";
     return {txnId, txnTime, seqNo, timestamp, type}
 }
