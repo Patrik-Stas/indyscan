1a506350cb6d6c3bc5b6c442e89eb7aa5fa60e7c
Patrik Stas <patrik.stas@absa.co.za>
2019-01-07 22:05:16 +0100

    Make building/pushing images easier.Add docker compose for nginx

--- build-daemon.sh
+++ build-daemon.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-TIME=`date -u +%Y%m%dT%H%M%SZ`
-REPO="pstas/indyscan-daemon"
-IMAGE_TAG="$REPO:latest"
-
-echo "Building docker image '$IMAGE_TAG'"
-docker build -t "$IMAGE_TAG" -f daemon/Dockerfile .
+./build-and-push.sh "indyscan-daemon"
