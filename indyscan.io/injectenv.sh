810e10d582de11e2e8b3404ebec9c924a806cdac
Patrik Stas <patrik.stas@gmail.com>
2019-05-26 16:33:24 +0200

    Remove indyscan.io specific files

--- indyscan.io/injectenv.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# Run this:
-# eval `./injectenv.sh`
-RELATIVE_CONFIG_PATH="$(dirname "$0")/webapp-config.json"
-CONFIG_WITH_NEWLINES=`cat $RELATIVE_CONFIG_PATH`
-CONFIG_NO_NEWLINES=`echo $CONFIG_WITH_NEWLINES`
-echo "export INDY_NETWORKS_V2='$CONFIG_NO_NEWLINES'"
