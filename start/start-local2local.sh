cc54a41177d748ef591a26595e213dc3d521a9d2
Patrik Stas <patrik.stas@gmail.com>
2019-06-13 11:42:08 +0200

    Build images by default when running indyscan+pool locally

--- start/start-local2local.sh
+++ start/start-local2local.sh
@@ -3,11 +3,11 @@ set -e
 
 SCRIPT_DIR_PATH=$(dirname "$0")
 
-NETWORK_NAME="indypool-by-indyscan-x"
+NETWORK_NAME="indypool-by-indyscan-x2"
 
 "$SCRIPT_DIR_PATH"/indypool/start.sh --address "127.0.0.1" --version "v1.8.3" --name "$NETWORK_NAME"
-"$SCRIPT_DIR_PATH"/indyscan-daemon/start.sh --mode 'download' --url-mongo "mongodb://localhost:27999" --indy-networks "$NETWORK_NAME"
-"$SCRIPT_DIR_PATH"/indyscan-webapp/start.sh --mode 'download' --url-mongo "mongodb://mongo.indynet:27017" --indy-networks "$NETWORK_NAME"
+"$SCRIPT_DIR_PATH"/indyscan-daemon/start.sh --mode 'build' --url-mongo "mongodb://localhost:27999" --indy-networks "$NETWORK_NAME" -d
+"$SCRIPT_DIR_PATH"/indyscan-webapp/start.sh --mode 'build' --url-mongo "mongodb://mongo.indynet:27017" --indy-networks "$NETWORK_NAME"
 
 
 
