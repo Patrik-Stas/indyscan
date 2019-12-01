e4b479d7332a42eb7efccc07c17ffe2a3366cd64
Patrik Stas <patrik.stas@gmail.com>
2019-12-01 17:19:20 +0100

    Add file-storoge back for scanner testing

--- indyscan-storage/src/index.js
+++ indyscan-storage/src/index.js
@@ -1,13 +1,15 @@
 const { createStorageMongo } = require('./mongo/storage-mongo')
 const { createMongoCollection } = require('./factory')
 const { createStorageEs } = require('./es/storage-es')
+const { createStorageFs } = require('./fs/storage-fs')
 const mongoTxFilters = require('./mongo/filter-builder')
 const esTxFilters = require('./es/es-query-builder')
 
 module.exports = {
+  createStorageFs,
   createStorageMongo,
   createStorageEs,
   createMongoCollection,
   mongoTxFilters,
-  esTxFilters,
+  esTxFilters
 }
