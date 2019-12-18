1c0770af4e762cf1192043e61207f59633213bbb
Patrik Stas <patrik.stas@gmail.com>
2019-12-18 21:24:55 +0100

    Optimize base indysdk docker iamge, remove extra logfile

--- ubuntu-libindy/build.sh
+++ ubuntu-libindy/build.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
 SCRIPT_DIR_PATH=$(dirname "$0")
 
-INDY_VERSION="v1.10.0"
-DOCKER_BUILD_ARGS="INDY_VERSION=$INDY_VERSION"
-docker build --build-arg "$DOCKER_BUILD_ARGS" -t "ubuntu-libindy:$INDY_VERSION" -f "$SCRIPT_DIR_PATH/Dockerfile" .
+INDYSDK_REPO_OWNER="hyperledger"
+INDYSDK_REVISION="v1.13.0"
+NAME=""
+
+DOCKER_TAG="indyscan-indysdk:$INDYSDK_REPO_OWNER-$INDYSDK_REVISION"
+
+if [[ "$NAME" ]]; then
+  DOCKER_TAG="$DOCKER_TAG-$NAME"
+fi;
+
+INDYSDK_REPO="https://github.com/${INDYSDK_REPO_OWNER}/indy-sdk"
+
+echo "Going to build $DOCKER_TAG from revision $INDYSDK_REVISION at repo: $INDYSDK_REPO"
+echo "Do you want to continue? (y/n)"
+read yesno
+if [ $yesno != "y" ]; then
+  exit 0
+fi
+
+docker build --build-arg "INDYSDK_REPO=$INDYSDK_REPO" \
+             --build-arg "INDYSDK_REVISION=$INDYSDK_REVISION" \
+             -t "$DOCKER_TAG" \
+             -f "$SCRIPT_DIR_PATH/Dockerfile" \
+             .
+
 
