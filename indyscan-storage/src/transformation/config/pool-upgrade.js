b1108759039ef20c4b70700e7598d6fb064bdc44
Patrik Stas <patrik.stas@gmail.com>
2020-01-06 07:45:44 +0100

    Workaround fix for stagingnet/config/24933 corrupted tx

--- indyscan-storage/src/transformation/config/pool-upgrade.js
+++ indyscan-storage/src/transformation/config/pool-upgrade.js
@@ -4,6 +4,10 @@ async function transformPoolUpgrade (tx) {
     tx.txn.data.schedule = []
     for (const scheduleKey of Object.keys(originalSchedule)) {
       let scheduleTime = originalSchedule[scheduleKey]
+      if (scheduleKey === 'BLu5t8JVbpHrRrocSx1HtMqJC8xruDLisaYZMZverkBs' &&
+        scheduleTime === '2019-07-15T10:11.555000-06:00') {
+        scheduleTime = '2019-07-15T10:11:00.555000-06:00' // corrupted format staging net config transaction 24933
+      }
       tx.txn.data.schedule.push({ scheduleKey, scheduleTime })
     }
   }
--- indyscan-storage/test/unit/transformation/config-subledger/transform-tx-config-pool-upgrade.spec.js
+++ indyscan-storage/test/unit/transformation/config-subledger/transform-tx-config-pool-upgrade.spec.js
@@ -1,6 +1,7 @@
 /* eslint-env jest */
 const { createIndyscanTransform } = require('../../../../src/transformation/transform-tx')
 const txPoolUpgrade = require('../../../resource/sample-txs/tx-config-pool-upgrade')
+const txPoolUpgradeCorrupted = require('../../../resource/sample-txs/tx-config-pool-upgrade-corrupted')
 
 let esTransform = createIndyscanTransform((seqno) => { throw Error(`Domain tx lookup seqno=${seqno} was not expected.`) })
 
@@ -20,5 +21,17 @@ describe('config/pool-upgrade transaction transformations', () => {
     expect(transformed.subledger.code).toBe(CONFIG_LEDGER_ID)
     expect(transformed.subledger.name).toBe('CONFIG')
     expect(transformed.txnMetadata.txnTime).toBe('2019-11-11T14:23:24.000Z')
+  })
+
+  it('should process corrupted tx stagingnet/config/24933 with wrong schedule time', async () => {
+    let transformed = await esTransform(txPoolUpgradeCorrupted, 'CONFIG')
+    expect(transformed.txn.typeName).toBe('POOL_UPGRADE')
+    expect(Array.isArray(transformed.txn.data.schedule)).toBeTruthy()
+    expect(transformed.txn.data.schedule.length).toBe(15)
+    let scheduleHash = 'BLu5t8JVbpHrRrocSx1HtMqJC8xruDLisaYZMZverkBs'
+    let scheduleRecords = transformed.txn.data.schedule.filter(r => r.scheduleKey === scheduleHash)
+    expect(scheduleRecords.length).toBe(1)
+    expect(scheduleRecords[0].scheduleKey).toBe('BLu5t8JVbpHrRrocSx1HtMqJC8xruDLisaYZMZverkBs')
+    expect(scheduleRecords[0].scheduleTime).toBe('2019-07-15T10:11:00.555000-06:00')
   })
 })
