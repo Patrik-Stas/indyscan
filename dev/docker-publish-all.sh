ad15968c04dc4d497716d89c0aef76dc339f4c2f
Patrik Stas <patrik.stas@gmail.com>
2020-03-15 09:49:10 +0100

    Add prototypes for json config file format

--- dev/docker-publish-all.sh
+++ dev/docker-publish-all.sh
@@ -7,5 +7,7 @@ echo "Docker publish for indyscan-daemon-ui"
 (cd ../indyscan-daemon-ui && yes | ./dockerpublish.sh)
 echo "Docker publish for indyscan-webapp"
 (cd ../indyscan-webapp && yes | ./dockerpublish.sh)
+echo "Docker publish for indyscan-webapp"
+(cd ../start/indypool && yes | ./dockerpublish.sh)
 
 
