2dd412648490d8a0271a4bb0557a893b50b5ea9b
Patrik Stas <patrik.stas@gmail.com>
2020-04-05 20:09:20 +0200

    Assure correct display of network header, description and logo

--- indyscan-webapp/components/NetworkInfo/NetworkInfo.js
+++ indyscan-webapp/components/NetworkInfo/NetworkInfo.js
@@ -8,23 +8,60 @@ class NetworkInfo extends Component {
     this.props = props
   }
 
-  render () {
+  getNetworkCaptionText () {
     const { networkDetails } = this.props
+    return (networkDetails.ui['display-long'])
+      ? networkDetails.ui['display-long']
+      : (networkDetails.ui['display'])
+        ? networkDetails.ui['display']
+        : networkDetails.id
+  }
 
+  getNetworkImage () {
+    const { networkDetails } = this.props
+    if (networkDetails && networkDetails.ui) {
+      return networkDetails.ui['logo-address']
+    }
+    return '/static/hlindy.png'
+  }
+
+  getNetworkDescription () {
+    const { networkDetails } = this.props
+    if (networkDetails && networkDetails.ui) {
+      return networkDetails.ui.description
+    }
+    return ''
+  }
+
+  getNetworkInstructions () {
+    const { networkDetails } = this.props
+    if (networkDetails && networkDetails.ui) {
+      return networkDetails.ui.tutorial
+    }
+    return ''
+  }
+
+  getNetworTutorialLink () {
+    const { networkDetails } = this.props
+    if (networkDetails && networkDetails.ui) {
+      return networkDetails.ui['tutorial-link']
+    }
+    return undefined
+  }
+
+  render () {
+    const tutorialLink = this.getNetworTutorialLink()
     return (
       <Grid style={{ backgroundColor: 'white', marginTop: '1em' }}>
-        <GridRow >
+        <GridRow>
           <GridColumn>
             <h1 className="network-header">
-              <img style={{ height: '1.5em', marginRight:'1em', marginBottom:'-0.3em' }} src="/static/sovrin.png" alt="Sovrin logo"/>{networkDetails.ui['display-long'] ? networkDetails.ui['display-long'] : (networkDetails.ui['display']) ? networkDetails.ui['display'] : networkDetails.id}
+              <img style={{ height: '1.5em', marginRight: '1em', marginBottom: '-0.3em' }} src={this.getNetworkImage()}
+                   alt="Sovrin logo"/>{this.getNetworkCaptionText()}
             </h1>
             <ul className="home-networkinfo">
-              {
-                (networkDetails.ui.description) && <li>{networkDetails.ui.description}</li>
-              }
-              {
-                (networkDetails.ui.tutorial) && <li>{networkDetails.ui.tutorial}<a href={networkDetails.ui["tutorial-link"]}> here</a></li>
-              }
+              <li>{this.getNetworkDescription()}</li>
+              {tutorialLink && <li>{this.getNetworkInstructions()} <a href={tutorialLink}> here</a></li>}
             </ul>
           </GridColumn>
         </GridRow>
