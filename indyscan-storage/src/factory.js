9d329c908e304cd6259b52895e8a8e37df9ae246
Patrik Stas <patrik.stas@gmail.com>
2021-07-06 12:14:58 +0200

    More dependency updates
    Signed-off-by: Patrik Stas <patrik.stas@gmail.com>

--- indyscan-storage/src/factory.js
+++
@@ -1,5 +0,0 @@
-module.exports.createMongoCollection = async function createMongoCollection (mongoDatabase, collectionName) {
-  const collection = await mongoDatabase.collection(collectionName)
-  await collection.createIndex({ 'txnMetadata.seqNo': 1 })
-  return collection
-}
